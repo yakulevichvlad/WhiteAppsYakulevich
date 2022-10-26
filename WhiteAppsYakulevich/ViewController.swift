4d2ebc14a67b9b1b2a5de99f27a39366acbfa9c7
yakulevichvlad <116755039+yakulevichvlad@users.noreply.github.com>
2022-10-26 21:55:50 +0300

    initial

---
+++ WhiteAppsYakulevich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteAppsYakulevich
+//
+//  Created by Vladislav Yakulevich on 26.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
