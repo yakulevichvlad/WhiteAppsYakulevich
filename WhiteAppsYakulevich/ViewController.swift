97cec2c8acd49917ba0f6b32f3ba7421ec099deb
yakulevichvlad <116755039+yakulevichvlad@users.noreply.github.com>
2022-10-27 23:35:22 +0300

    результат урока stage1_lesson2

--- WhiteAppsYakulevich/ViewController.swift
+++ WhiteAppsYakulevich/ViewController.swift
@@ -2,18 +2,50 @@
 //  ViewController.swift
 //  WhiteAppsYakulevich
 //
-//  Created by Vladislav Yakulevich on 26.10.22.
+//  Created by Vladislav Yakulevich on 27.10.22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    
+    let name = "Меня зовут Влад."
+    let ageTitle = "Мне"
+    var ageCount = 17
+    let ageWord = "лет."
+    let height  = "Мой рост 180, Вес 68кг"
+    let mouse = "Мышка Logitech, Цвет белый, весит"
+    var mouseWeight = 200
+    let mouseGrams = "грамм."
+    let headPh = "Беспрводные Наушники, Работают от одной зарядки"
+    var headPhTime = 6
+    let headPhWord = "часов."
+    let digMonitor = "Монитор Dell 24 дюйма, 60 герц"
+    
+    lazy var me = "\(name) \(ageTitle) \(ageCount) \(ageWord) \(height)"
+    lazy var mouseSetup = "\(mouse) \(mouseWeight) \(mouseGrams)"
+    lazy var headPhTitle = "\(headPh) \(headPhTime) \(headPhWord)"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        print(me)
+        print(mouseSetup)
+        print(headPhTitle)
+        print(digMonitor)
     }
+    
+    
+
+  
+    }
+    
+    
+    
 
 
-}
+
 
