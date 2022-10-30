a4cbe67b58f03bfc887ed1cde6aba044e25fba78
yakulevichvlad <116755039+yakulevichvlad@users.noreply.github.com>
2022-10-30 22:43:05 +0300

    результат урока stage1_lesson3

--- WhiteAppsYakulevich/ViewController.swift
+++ WhiteAppsYakulevich/ViewController.swift
@@ -9,38 +9,21 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    
-    let name: String = "Меня зовут Влад."
-    let ageTitle = "Мне"
-    var ageCount: Int = 17
-    let ageWord = "лет."
-    let height  = "Мой рост 180, Вес 68кг"
-    let mouse = "Мышка Logitech, Цвет белый, весит"
-    var mouseWeight: Int = 200
-    let mouseGrams = "грамм."
-    let headPh = "Беспрводные Наушники, Работают от одной зарядки"
-    var headPhTime: Int = 6
-    let headPhWord = "часов."
-    let digMonitor: String = "Монитор Dell 24 дюйма, 60 герц"
-    
-
-    
-    lazy var me = "\(name) \(ageTitle) \(ageCount) \(ageWord) \(height)"
-    lazy var mouseSetup = "\(mouse) \(mouseWeight) \(mouseGrams)"
-    lazy var headPhTitle = "\(headPh) \(headPhTime) \(headPhWord)"
-    
+    let a = 2+2
+    let b = 3+3
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(me)
-        print(mouseSetup)
-        print(headPhTitle)
-        print(digMonitor)
+        
+        if a < b {
+        print(true)
+        } else {
+            print(false)
+        
     }
     
-    
+    }
 
   
     }
